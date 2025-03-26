b140199be138dba895f2323b27d5d5565d44e91d
gaucho212 <adamwoz2005@gmail.com>
2025-03-26 13:52:22 +0100

    Improved Remove add function checking if there is key in the tree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+
 # Tworzenie drzewa BST
 class BSTNode:
     def __init__(self, key):
@@ -7,10 +8,12 @@ class BSTNode:
         self.left = None
         self.right = None
 
+
 # Implementacje komend w BST
 
+
 class BSTree:
-    #Dodawanie elementu do drzewa
+    # Dodawanie elementu do drzewa
     def insert(self, root, key):
         if not root:
             return BSTNode(key)
@@ -19,45 +22,53 @@ class BSTree:
         else:
             root.right = self.insert(root.right, key)
         return root
-    
-#Wypisawywanie drzewa 
 
-    #In-order
+    # Wypisawywanie drzewa
+
+    # In-order
     def in_order(self, root):
         if not root:
             return
         self.in_order(root.left)
         print(root.key, end=" ")
         self.in_order(root.right)
-        
-    #Pre-order
+
+    # Pre-order
     def pre_order(self, root):
         if not root:
             return
         print(root.key, end=" ")
         self.pre_order(root.left)
         self.pre_order(root.right)
-        
-    #Post-order
+
+    # Post-order
     def post_order(self, root):
         if not root:
             return
         self.post_order(root.left)
         self.post_order(root.right)
         print(root.key, end=" ")
-        
-    #Rysowanie drzewa
-    def export(self,root,level=0):
-        indent = "    " * level  
+
+    # Rysowanie drzewa
+    def export(self, root, level=0):
+        indent = "    " * level
         if not root.left and not root.right:
             return f"{indent}leaf {root.key}"
-        
-        l_str = self.export(root.left, level+1) if root.left else f"{indent}    child [missing]"
-        r_str = self.export(root.right, level+1) if root.right else f"{indent}    child [missing]"
-        
+
+        l_str = (
+            self.export(root.left, level + 1)
+            if root.left
+            else f"{indent}    child [missing]"
+        )
+        r_str = (
+            self.export(root.right, level + 1)
+            if root.right
+            else f"{indent}    child [missing]"
+        )
+
         return f"{indent}node {root.key}\n{l_str} \n{r_str}"
-        
-    #Znajdowanie minimalnej i maksymalnej wartości w drzewie
+
+    # Znajdowanie minimalnej i maksymalnej wartości w drzewie
     def find_min(self, root):
         while root.left:
             root = root.left
@@ -67,30 +78,31 @@ class BSTree:
         while root.right:
             root = root.right
         return root
-    
+
     # Usuwanie elementów
     def remove(self, root, key):
         if not root:
             return root
-        
+
         if key < root.key:
             root.left = self.remove(root.left, key)
 
         elif key > root.key:
             root.right = self.remove(root.right, key)
-            
+
         else:
             if not root.left:
                 return root.right
             elif not root.right:
                 return root.left
-            
+
             temp = self.find_min(root.right)
             root.key = temp.key
-            root.right = self.remove(root.right , temp.key)
+            root.right = self.remove(root.right, temp.key)
 
         return root
-    
+
+
 # Tworzenie drzewa AVL
 class AVLNode:
     def __init__(self, key):
@@ -99,10 +111,11 @@ class AVLNode:
         self.right = None
         self.height = 1
 
+
 # Implementacje komend w AVL
 class AVLTree:
-    
-    #Dodawanie elementow do drzewa i balansowanie drzewa
+
+    # Dodawanie elementow do drzewa i balansowanie drzewa
     def insert(self, root, key):
         if not root:
             return AVLNode(key)
@@ -127,7 +140,7 @@ class AVLTree:
 
         return root
 
-    #Znajdowanie minimalnej i maksymalnej wartości w drzewie
+    # Znajdowanie minimalnej i maksymalnej wartości w drzewie
     def find_min(self, root):
         while root.left:
             root = root.left
@@ -137,8 +150,8 @@ class AVLTree:
         while root.right:
             root = root.right
         return root
-    
-    #Rotacje lewe i prawe
+
+    # Rotacje lewe i prawe
     def left_rotate(self, z):
         y = z.right
         T2 = y.left
@@ -156,19 +169,19 @@ class AVLTree:
         z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
         y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
         return y
-    
-    #Wysokość drzewa
+
+    # Wysokość drzewa
     def get_height(self, root):
         if not root:
             return 0
         return root.height
-    
-    #Balansowanie drzewa
+
+    # Balansowanie drzewa
     def get_balance(self, root):
         if not root:
             return 0
         return self.get_height(root.left) - self.get_height(root.right)
-    
+
     # Usuwanie elementu z drzewa i balansowanie drzewa
     def remove(self, root, key):
         if not root:
@@ -206,52 +219,60 @@ class AVLTree:
             return self.left_rotate(root)
 
         return root
-    
-#Wypisywanie drzewa
 
-    #In-order
+    # Wypisywanie drzewa
+
+    # In-order
     def in_order(self, root):
         if not root:
             return
         self.in_order(root.left)
         print(root.key, end=" ")
         self.in_order(root.right)
-        
-    #Pre-order
+
+    # Pre-order
     def pre_order(self, root):
         if not root:
             return
         print(root.key, end=" ")
         self.pre_order(root.left)
         self.pre_order(root.right)
-        
-    #Post-order
+
+    # Post-order
     def post_order(self, root):
         if not root:
             return
         self.post_order(root.left)
         self.post_order(root.right)
         print(root.key, end=" ")
-    
-    #Rysowanie drzewa
-    def export(self,root,level=0,type="ROOT"):
-        indent = "    " * level  
+
+    # Rysowanie drzewa
+    def export(self, root, level=0, type="ROOT"):
+        indent = "    " * level
         if not root.left and not root.right:
             return f"{indent}{type} LEAF {root.key}"
-        
-        l_str = self.export(root.left, level+1,"LEFT") if root.left else f"{indent}    child [missing]"
-        r_str = self.export(root.right, level+1,"RIGHT") if root.right else f"{indent}    child [missing]"
-        
+
+        l_str = (
+            self.export(root.left, level + 1, "LEFT")
+            if root.left
+            else f"{indent}    child [missing]"
+        )
+        r_str = (
+            self.export(root.right, level + 1, "RIGHT")
+            if root.right
+            else f"{indent}    child [missing]"
+        )
+
         return f"{indent}{type} {root.key}\n{l_str} \n{r_str}"
 
 
 def main():
-        
+
     if "--tree" == sys.argv[1] and "AVL" == sys.argv[2]:
         tree = AVLTree()
         root = None
         inserted = []
-        
+
         print("Enter values to insert into the AVL tree:")
         s = sys.stdin.read()
         value = list(map(int, s.split()))
@@ -262,12 +283,12 @@ def main():
 
         print("Tree is completed")
         print()
-        
+
     elif "--tree" == sys.argv[1] and "BST" == sys.argv[2]:
         tree = BSTree()
         root = None
         inserted = []
-        
+
         print("Enter values to insert into the BST tree:")
         s = sys.stdin.read()
         value = list(map(int, s.split()))
@@ -278,7 +299,7 @@ def main():
 
         print("Tree is completed")
         print()
-        
+
     else:
         print("Wrong arguments. Please use the following format:")
         print("python main.py --tree 'AVL/BST'")
@@ -301,7 +322,7 @@ def main():
     while True:
         print("action> ", end="")
         action = input()
-        
+
         if action == "Help":
             print(practice)
 
@@ -313,19 +334,42 @@ def main():
             print("\nPost-order: ", end="")
             tree.post_order(root)
             print()
-            
+
         elif action == "FindMinMax":
             print("Min: ", tree.find_min(root).key, "\nMax: ", tree.find_max(root).key)
 
         elif action == "Remove":
             print("to remove> ", end="")
             values = list(map(int, input().split()))
+            removed = []
+            not_found = []
+
+            # Funkcja pomocnicza do sprawdzania czy element istnieje w drzewie
+            def find_key(root, key):
+                if not root:
+                    return False
+                if root.key == key:
+                    return True
+                if key < root.key:
+                    return find_key(root.left, key)
+                return find_key(root.right, key)
+
             for value in values:
-                root = tree.remove(root, value)
-            print(f"Removed {values} from the tree.")
+                if find_key(root, value):
+                    root = tree.remove(root, value)
+                    removed.append(value)
+                else:
+                    not_found.append(value)
+
+            if removed:
+                print(f"Removed {removed} from the tree.")
+            if not_found:
+                print(f"Values {not_found} not found in the tree.")
+            if not removed and not not_found:
+                print("No values provided to remove.")
 
         elif action == "Draw":
-            print( f"\\{tree.export(root)}")
+            print(f"\\{tree.export(root)}")
 
         elif action == "Exit":
             break
